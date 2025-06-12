3d27fe9df6eb5de81fde22ea2f7a0f4ff1c74d4a
Sumaiya Khan <eng.sumaiyakhan@gmail.com>
2025-06-12 11:00:23 +0530

    solve the error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 import RegisterForm from '../app/register/page'
-import LoginForm from '../app/login/page'
-import { useState } from 'react';
+
 export default function Home() {
-  // const [isLogin, setIsLogin] = useState(true)
+ 
   return (
     
       <div className='main-pages-container'>
