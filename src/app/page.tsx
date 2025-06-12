983d708a084a0aa4470dfd301cc3b45302a78432
Sumaiya Khan <eng.sumaiyakhan@gmail.com>
2025-06-12 10:56:47 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from '../app/register/page'
 import LoginForm from '../app/login/page'
 import { useState } from 'react';
 export default function Home() {
-  const [isLogin, setIsLogin] = useState(true)
+  // const [isLogin, setIsLogin] = useState(true)
   return (
     
       <div className='main-pages-container'>
